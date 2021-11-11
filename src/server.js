29f1646e25fe00973f9d559b083bf09bcbef61fc
lyh7712 <mmj2rv9@kakao.com>
2021-11-11 17:48:53 +0900

    middlerware

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import morgan from "morgan";
 
 const app = express(); // express 생성
+const loggerMiddleware = morgan("dev");
 
-const PORT = 4003;
+const PORT = 4000;
 
-app.get('/', () => 'Somebody come Home'); // GET 요청시 이벤트
+const handleHome = (req, res) => {
+  return res.send('Hello Home');
+};
+
+const handleLogin = (req, res) => {
+  res.send('Here Login');
+};
+
+app.use(loggerMiddleware);
+
+app.get('/', handleHome); // GET 요청시 이벤트
+app.get('/login', handleLogin);
 
 const handleListening = () => console.log(`Server listeting on port ${PORT}`);
 
